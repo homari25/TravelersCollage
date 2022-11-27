da1b0c35d566aa82d600e1a6f29374f41e69e592
homari25 <t09850mh@gmail.com>
2022-11-27 16:47:43 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 let how = document.getElementById("how");
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 how.onclick = function() {
