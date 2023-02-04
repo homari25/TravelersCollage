b260cbb97be4aa3156e1a390f0a85283e796e123
homari25 <t09850mh@gmail.com>
2023-02-04 11:27:00 -0500

    update

--- collection/scripts.js
+++ collection/scripts.js
@@ -25,6 +25,9 @@ let colorsList = document.getElementById("colorList");
 let docList = document.getElementById("documentList");
 let senseList = document.getElementById("senseList");
 
+let randomPosX = Math.random() * 10;
+let randomPosY = Math.random() * 10;
+console.log(randomPosX);
 
 function submit() {
     let colorSelect = colorList.options[colorList.selectedIndex].text;  
@@ -38,25 +41,26 @@ for (let i = 0; i < imgResult.length; i++) {
     let docType = imgResult[i].getAttribute("data-doc");
     let senseType = imgResult[i].getAttribute("data-sense");
 
-    if (colorType == colorSelect && docType == docSelect && senseType == senseSelect) {
+    if (colorType == colorSelect) {
       imgResult[i].style.display = "block";
-      imgResult[i].style.left = 10 * i + "px";
+      imgResult[i].style.top = randomPosY * i+ "px";
+      imgResult[i].style.left = randomPosX * i + "px";
         } else {
           imgResult[i].style.display = "none";
         }
-    if (docType == docSelect) {
-      imgResult[i].style.display = "block";
-      imgResult[i].style.left = 10 * i + "px";
-        } else {
-          imgResult[i].style.display = "none";
-    }
-    if (senseType == senseSelect) {
-      imgResult[i].style.display = "block";
-      imgResult[i].style.left = 10 * i + "px";        
-    } else {
-      imgResult[i].style.display = "none";
-        }
-
-    }
+    // if (docType == docSelect) {
+    //   imgResult[i].style.display = "block";
+    //   imgResult[i].style.left = 10 * i + "px";
+    //     } else {
+    //       imgResult[i].style.display = "none";
+    // }
+    // if (senseType == senseSelect) {
+    //   imgResult[i].style.display = "block";
+    //   imgResult[i].style.left = i + "px";
+    //   imgResult[i].style.top = i + "px";
+    // } else {
+    //   imgResult[i].style.display = "none";
+    //     }
+      }
 }
 
