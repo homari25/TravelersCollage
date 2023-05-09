0e6f75646c0c395c99f2e2a1aace956fb444683a
homari25 <t09850mh@gmail.com>
2023-05-08 23:40:35 -0400

    fixed collection

--- collection/scripts.js
+++ collection/scripts.js
@@ -4,80 +4,79 @@ let docList = document.getElementById("documentList");
 let senseList = document.getElementById("senseList");
 let selected = document.getElementsByClassName("selected");
 let container = document.getElementById("container");
-let height = window.innerHeight
+let height = window.innerHeight;
 let width = window.innerWidth;
 
-
-let randomPosX = Math.random() * 10;
-// container.style.backgroundColor = "blue";
-
 function submit() {
-    let colorSelect = colorList.options[colorList.selectedIndex].value;  
-    let docSelect = documentList.options[documentList.selectedIndex].value;  
-    let senseSelect = senseList.options[senseList.selectedIndex].value;  
-    console.log(colorSelect,docSelect,senseSelect);
-    document.getElementById("container").className = "container";
+  let colorSelect = colorList.options[colorList.selectedIndex].value;  
+  let docSelect = documentList.options[documentList.selectedIndex].value;  
+  let senseSelect = senseList.options[senseList.selectedIndex].value;  
+  console.log(colorSelect,docSelect,senseSelect);
+  document.getElementById("container").className = "container";
 
-for (let i = 0; i < imgResults.length; i++) {
+  let count = 0;
+  for (let i = 0; i < imgResults.length; i++) {
     let colorType = imgResults[i].getAttribute("data-color");
-    // for (imgResult of imgResults) {
-      if (colorType == colorSelect) {
+    let docType = imgResults[i].getAttribute("data-doc");
+    let senseType = imgResults[i].getAttribute("data-sense"); 
+
+    // Check if any filter is selected
+    let filterApplied = (colorSelect !== "none") || (docSelect !== "none") || (senseSelect !== "none");
+
+    if (!filterApplied || (colorType == colorSelect && docType == docSelect && senseType == senseSelect)) {
       imgResults[i].style.display = "block";
       imgResults[i].style.position = "absolute";
-      if (i%3 == 0) {
-        imgResults[i].style.top = 0 + "px";
-        imgResults[i].style.left = i * Math.random() * 10 + "px";
-      } else if (i%3 == 1) {
-        imgResults[i].style.top = 250 + "px";
-        imgResults[i].style.left = i * Math.random() * (45 - 20) + "px";
-      } else {
-        imgResults[i].style.top = 500 + "px";
-        imgResults[i].style.left = i * Math.random() * (90-50) + "px";
-      }
+      imgResults[i].style.width = "10%";
+      imgResults[i].style.top = count % 3 * height / 3 + "px";
+      imgResults[i].style.left = Math.floor(count / 3) * width / 3 + "px";
+      count++;
     } else {
       imgResults[i].style.display = "none";
     }
   }
-    
-  for (let j = 0; j < imgResults.length; j++) {
-    let docType = imgResults[j].getAttribute("data-doc");
-    // for (imgResult of imgResults) {
-      if (docType == docSelect) {
-      imgResults[j].style.display = "block";
-      imgResults[j].style.position = "absolute";
-      if (j%3 == 0) {
-        imgResults[j].style.top = 0 + "px";
-        imgResults[j].style.left = j * Math.random() * 10 + "px";
-      } else if (j%3 == 1) {
-        imgResults[j].style.top = 250 + "px";
-        imgResults[j].style.left = j * Math.random() * (45 - 20) + "px";
-      } else {
-        imgResults[j].style.top = 500 + "px";
-        imgResults[j].style.left = j * Math.random() * (90-50) + "px";
-      }
-    } else {
-      imgResults[j].style.display = "none";
-    }
-  }
-
-  for (let k = 0; k < imgResults.length; k++) {
-    let senseType = imgResults[k].getAttribute("data-sense"); 
-    // for (imgResult of imgResults) {
-      if (senseType == senseSelect) {
-      imgResults[k].style.display = "block";
-      imgResults[k].style.position = "absolute";
-      if (k%3 == 0) {
-        imgResults[k].style.top = 0 + "px";
-        imgResults[k].style.left = k * Math.random() * 10 + "px";
-      } else if (k%3 == 1) {
-        imgResults[k].style.top = 250 + "px";
-        imgResults[k].style.left = k * Math.random() * (45 - 20) + "px";
-      } else {
-        imgResults[k].style.top = 500 + "px";
-        imgResults[k].style.left = k * Math.random() * (90-50) + "px";
-      }
-    } else {
-      imgResults[k].style.display = "none";
-    }
-  }
 }
+
+
+// let imgResults = document.getElementsByClassName("result");
+// let colorsList = document.getElementById("colorList");
+// let docList = document.getElementById("documentList");
+// let senseList = document.getElementById("senseList");
+// let selected = document.getElementsByClassName("selected");
+// let container = document.getElementById("container");
+// let height = window.innerHeight
+// let width = window.innerWidth;
+
+// function submit() {
+//   let colorSelect = colorList.options[colorList.selectedIndex].value;  
+//   let docSelect = documentList.options[documentList.selectedIndex].value;  
+//   let senseSelect = senseList.options[senseList.selectedIndex].value;  
+//   console.log(colorSelect, docSelect, senseSelect);
+//   document.getElementById("container").className = "container";
+
+//   for (let i = 0; i < imgResults.length; i++) {
+//     let colorType = imgResults[i].getAttribute("data-color");
+//     let docType = imgResults[i].getAttribute("data-doc");
+//     let senseType = imgResults[i].getAttribute("data-sense"); 
+
+//     // Check if any filter is selected
+//     let filterApplied = (colorSelect !== "none") || (docSelect !== "none") || (senseSelect !== "none");
+
+//     if (!filterApplied || (colorSelect !== "none" && colorType == colorSelect) ||
+//         (docSelect !== "none" && docType == docSelect) || (senseSelect !== "none" && senseType == senseSelect)) {
+//       imgResults[i].style.display = "block";
+//       imgResults[i].style.position = "absolute";
+//       if (i%3 == 0) {
+//         imgResults[i].style.top = 0 + "px";
+//         imgResults[i].style.left = i * Math.random() * 10 + "px";
+//       } else if (i%3 == 1) {
+//         imgResults[i].style.top = 250 + "px";
+//         imgResults[i].style.left = i * Math.random() * (45 - 20) + "px";
+//       } else {
+//         imgResults[i].style.top = 500 + "px";
+//         imgResults[i].style.left = i * Math.random() * (90-50) + "px";
+//       }
+//     } else {
+//       imgResults[i].style.display = "none";
+//     }
+//   }
+// }
