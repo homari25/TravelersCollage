49390283558c1cf7204ef982e90f76a2877cc9b0
homari25 <t09850mh@gmail.com>
2023-05-08 21:11:26 -0400

    collection update

--- collection/scripts.js
+++ collection/scripts.js
@@ -1,4 +1,4 @@
-let imgResult = document.getElementsByClassName("result");
+let imgResults = document.getElementsByClassName("result");
 let colorsList = document.getElementById("colorList");
 let docList = document.getElementById("documentList");
 let senseList = document.getElementById("senseList");
@@ -18,43 +18,66 @@ function submit() {
     console.log(colorSelect,docSelect,senseSelect);
     document.getElementById("container").className = "container";
 
-for (let i = 0; i < imgResult.length; i++) {
-    let colorType = imgResult[i].getAttribute("data-color");
-    // console.log(colorType);
-    let docType = imgResult[i].getAttribute("data-doc");
-    let senseType = imgResult[i].getAttribute("data-sense");
+for (let i = 0; i < imgResults.length; i++) {
+    let colorType = imgResults[i].getAttribute("data-color");
+    // for (imgResult of imgResults) {
+      if (colorType == colorSelect) {
+      imgResults[i].style.display = "block";
+      imgResults[i].style.position = "absolute";
+      if (i%3 == 0) {
+        imgResults[i].style.top = 0 + "px";
+        imgResults[i].style.left = i * Math.random() * 10 + "px";
+      } else if (i%3 == 1) {
+        imgResults[i].style.top = 250 + "px";
+        imgResults[i].style.left = i * Math.random() * (45 - 20) + "px";
+      } else {
+        imgResults[i].style.top = 500 + "px";
+        imgResults[i].style.left = i * Math.random() * (90-50) + "px";
+      }
+    } else {
+      imgResults[i].style.display = "none";
+    }
+  }
+    
+  for (let j = 0; j < imgResults.length; j++) {
+    let docType = imgResults[j].getAttribute("data-doc");
+    // for (imgResult of imgResults) {
+      if (docType == docSelect) {
+      imgResults[j].style.display = "block";
+      imgResults[j].style.position = "absolute";
+      if (j%3 == 0) {
+        imgResults[j].style.top = 0 + "px";
+        imgResults[j].style.left = j * Math.random() * 10 + "px";
+      } else if (j%3 == 1) {
+        imgResults[j].style.top = 250 + "px";
+        imgResults[j].style.left = j * Math.random() * (45 - 20) + "px";
+      } else {
+        imgResults[j].style.top = 500 + "px";
+        imgResults[j].style.left = j * Math.random() * (90-50) + "px";
+      }
+    } else {
+      imgResults[j].style.display = "none";
+    }
+  }
 
-    if (colorType == colorSelect) {
-      // container.style.display = "grid";
-      // container.style.padding = 10 + "px";
-      // container.gridTemplateColumn = "auto auto auto";
-      for (j = 0; j < i; j++) {
-      imgResult[i].style.display = "block";
-      // imgResult[i].style.left = 5 * i + "px";
-      imgResult[i].style.top = Math.random() *  (200 - 20) + 20 + "px";
-      imgResult[i].style.left = Math.random() * (1000 - 50) + 50 + "px";
-      // imgResult[i].style.padding = 10 + "px";
-      imgResult[i].style.position = "absolute";
-      // imgResult[i].style.marginTop = 5 * i + "px";
-      // container.style.overflow = "hidden";
-      imgResult[i].style.paddingLeft = Math.random() * (300 - 20) + 20 + "px";
+  for (let k = 0; k < imgResults.length; k++) {
+    let senseType = imgResults[k].getAttribute("data-sense"); 
+    // for (imgResult of imgResults) {
+      if (senseType == senseSelect) {
+      imgResults[k].style.display = "block";
+      imgResults[k].style.position = "absolute";
+      if (k%3 == 0) {
+        imgResults[k].style.top = 0 + "px";
+        imgResults[k].style.left = k * Math.random() * 10 + "px";
+      } else if (k%3 == 1) {
+        imgResults[k].style.top = 250 + "px";
+        imgResults[k].style.left = k * Math.random() * (45 - 20) + "px";
+      } else {
+        imgResults[k].style.top = 500 + "px";
+        imgResults[k].style.left = k * Math.random() * (90-50) + "px";
       }
-        } else {
-          imgResult[i].style.display = "none";
-        }
-    // if (docType == docSelect) {
-    //   imgResult[i].style.display = "block";
-    //   imgResult[i].style.left = 10 * i + "px";
-    //     } else {
-    //       imgResult[i].style.display = "none";
-    // }
-    // if (senseType == senseSelect) {
-    //   imgResult[i].style.display = "block";
-    //   imgResult[i].style.left = i + "px";
-    //   imgResult[i].style.top = i + "px";
-    // } else {
-    //   imgResult[i].style.display = "none";
-    //     }
-      }
+    } else {
+      imgResults[k].style.display = "none";
+    }
+  }
 }
-
