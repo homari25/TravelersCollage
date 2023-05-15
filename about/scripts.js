27a40425da5127fa77386a203e24e3b67b880783
homari25 <t09850mh@gmail.com>
2023-05-14 23:26:31 -0400

    fix javascript

--- about/scripts.js
+++ about/scripts.js
@@ -2,6 +2,6 @@ let pictures = document.getElementsByClassName("picture");
 
 for (let picture of pictures) {
   let animationRandomSec = Math.random() * (9 - 5) + 5 + "s";
-  piecesdb.style.animationDuration = animationRandomSec;
-  piecesdb.style.display = "block";
+  picture.style.animationDuration = animationRandomSec;
+  picture.style.display = "block";
 }
