fcb08a4e0b5e9b9f59f486dc9744c0384915833b
homari25 <t09850mh@gmail.com>
2023-05-14 23:24:15 -0400

    Update scripts.js

--- about/scripts.js
+++ about/scripts.js
@@ -4,5 +4,4 @@ for (let picture of pictures) {
   let animationRandomSec = Math.random() * (9 - 5) + 5 + "s";
   piecesdb.style.animationDuration = animationRandomSec;
   piecesdb.style.display = "block";
-  console.log(piecesdb);
 }
